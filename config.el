944a95ff3cbc5a64adaa26ccf5643465d0fda10d
Alexey Shiklomanov <alexey.shiklomanov@gmail.com>
2019-07-20 01:18:17 -0400

    Fix more global keybindings

--- config.el
+++ config.el
@@ -4,10 +4,7 @@
 
 (setq doom-localleader-key "\\")
 
-(map! (:map (company-mode-map
-             :i "C-l" #'company-complete))
-
-      (:map evil-window-map
+(map! (:map evil-window-map
         "o" #'ace-window
         "z" #'doom/window-enlargen
         "-" #'evil-window-split
@@ -17,9 +14,49 @@
         "d" #'+workspace/close-window-or-workspace)
 
       (:map doom-leader-project-map
+        :desc "Find project file or buffer" "p" #'counsel-projectile
         :desc "Find file in project" "f" #'+ivy/projectile-find-file
         :desc "Switch project" "o" #'counsel-projectile-switch-project
         :desc ""
         "/" nil)
 
+      (:map doom-leader-workspace-map
+        :desc "Switch workspace" "TAB" #'+workspace/switch-to
+        :desc "Show tab bar" "." #'+workspace/display)
+
+      ;; (:map company-active-map
+      ;;   :i "C-l" #'+company/complete)
+
+      :i "C-l" #'+company/complete
       :n "g RET" #'eval-defun)
+
+
+;; Some custom functions
+(defun rename-this-buffer-and-file ()
+  "Renames current buffer and file it is visiting."
+  (interactive)
+  (let ((name (buffer-name))
+        (filename (buffer-file-name)))
+    (if (not (and filename (file-exists-p filename)))
+        (error "Buffer '%s' is not visiting a file!" name)
+      (let ((new-name (read-file-name "New name: " filename)))
+        (cond ((get-buffer new-name)
+               (error "A buffer named '%s' already exists!" new-name))
+              (t
+               (rename-file filename new-name 1)
+               (rename-buffer new-name)
+               (set-visited-file-name new-name)
+               (set-buffer-modified-p nil)
+               (message "File '%s' successfully renamed to '%s'" name (file-name-nondirectory new-name))))))))
+
+(evil-ex-define-cmd "rename" 'rename-this-buffer-and-file)
+
+(defun ans/delete-file-and-buffer ()
+  "Kill the current buffer and delete the associated file."
+  (interactive)
+  (let ((filename (buffer-file-name)))
+    (when filename
+      (progn
+        (delete-file filename)
+        (message "Deleted file %s" filename)
+        (kill-buffer)))))
