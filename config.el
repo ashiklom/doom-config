99f1de35c5ed884bf4f34d66b79389544d7d34c7
Alexey Shiklomanov <alexey.shiklomanov@gmail.com>
2019-07-19 23:11:18 -0400

    More keybinding modifications

--- config.el
+++ config.el
@@ -2,8 +2,24 @@
 
 ;; Place your private configuration here
 
+(setq doom-localleader-key "\\")
+
 (map! (:map (company-mode-map
              :i "C-l" #'company-complete))
+
       (:map evil-window-map
         "o" #'ace-window
-        "z" #'doom/window-enlargen))
+        "z" #'doom/window-enlargen
+        "-" #'evil-window-split
+        "\\" #'evil-window-vsplit
+        "+" #'evil-window-increase-height
+        "_" #'evil-window-decrease-height
+        "d" #'+workspace/close-window-or-workspace)
+
+      (:map doom-leader-project-map
+        :desc "Find file in project" "f" #'+ivy/projectile-find-file
+        :desc "Switch project" "o" #'counsel-projectile-switch-project
+        :desc ""
+        "/" nil)
+
+      :n "g RET" #'eval-defun)
