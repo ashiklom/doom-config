3c4c5a9a17932849f4b316bbff7d101d7c324567
Alexey Shiklomanov <alexey.shiklomanov@gmail.com>
2019-08-08 15:35:37 -0400

    Temporarily use my fork of helm-bibtex

--- modules/tools/bibtex/packages.el
+++ modules/tools/bibtex/packages.el
@@ -1,7 +1,8 @@
 ;; -*- no-byte-compile: t; -*-
 ;;; tools/bibtex/packages.el
 
-(package! helm-bibtex)
+;; (package! helm-bibtex)
+(package! helm-bibtex :recipe (:fetcher github :repo "ashiklom/helm-bibtex" :branch "author-abbrev"))
 (package! org-ref)
 (package! citeproc)
 (package! citeproc-org :recipe (:fetcher github :repo "andras-simonyi/citeproc-org"))
