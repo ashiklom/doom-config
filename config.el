ccb007bcfdbaea2f0757e42cae809da26d6d0dbc
Alexey Shiklomanov <alexey.shiklomanov@gmail.com>
2019-08-15 20:32:03 -0700

    Remove persp-mode saving fix
    Didn't really work anyway...

--- config.el
+++ config.el
@@ -282,25 +282,6 @@
 ;; Diary -- mostly, to prevent errors about this file not existing
 (setq diary-file "~/Dropbox/Notes/journal/diary")
 
-;; Persp-mode bugfix
-;; See issue https://github.com/hlissner/doom-emacs/issues/1525
-(after! persp-mode
-  (persp-def-buffer-save/load
-   :tag-symbol 'def-indirect-buffer
-   ;; :predicate #'buffer-base-buffer
-   :predicate (lambda (buf)
-                (and (buffer-base-buffer buf)
-                     (buffer-live-p buf)))
-   :save-function (lambda (buf tag vars)
-                    (list tag (buffer-name buf) vars
-                          (buffer-name (buffer-base-buffer buf))))
-   :load-function (lambda (savelist &rest _rest)
-                    (cl-destructuring-bind (buf-name _vars base-buf-name &rest _)
-                        (cdr savelist)
-                      (push (cons buf-name base-buf-name)
-                            +workspaces--indirect-buffers-to-restore)
-                      nil))))
-
 (use-package! fill-function-arguments
   :commands (fill-function-arguments-dwim))
 (map! :desc "Fill function arguments" :n "g [" #'ans/fill-function-arguements-and-indent)
