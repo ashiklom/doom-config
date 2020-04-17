1ff8ebce4526e6de413a1e44c88ad63e627c0373
Alexey Shiklomanov <alexey.shiklomanov@gmail.com>
2020-04-17 09:35:09 -0400

    Add bibtex-completion recipe

--- modules/tools/bibtex/packages.el
+++ modules/tools/bibtex/packages.el
@@ -1,6 +1,7 @@
 ;; -*- no-byte-compile: t; -*-
 ;;; tools/bibtex/packages.el
 
+(package! bibtex-completion :recipe (:host github :repo "tmalsburg/helm-bibtex" :files ("bibtex-completion.el")))
 (package! ivy-bibtex)
 (package! org-ref)
 (package! citeproc)
