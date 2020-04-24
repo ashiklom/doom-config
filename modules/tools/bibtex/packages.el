9cc32b47ede92d0541b8bbaca3b853f635cbcd7e
Alexey Shiklomanov <alexey.shiklomanov@gmail.com>
2020-04-24 09:39:34 -0400

    Disable citeproc-org

--- modules/tools/bibtex/packages.el
+++ modules/tools/bibtex/packages.el
@@ -4,5 +4,6 @@
 (package! bibtex-completion :recipe (:host github :repo "tmalsburg/helm-bibtex" :files ("bibtex-completion.el")))
 (package! ivy-bibtex)
 (package! org-ref)
-(package! citeproc)
-(package! citeproc-org :recipe (:host github :repo "andras-simonyi/citeproc-org"))
+
+(package! citeproc :disable t)
+(package! citeproc-org :recipe (:host github :repo "andras-simonyi/citeproc-org") :disable t)
