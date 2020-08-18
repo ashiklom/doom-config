5b5b2867ab7dba8e24508e1dd512a93da1302bed
Alexey Shiklomanov <alexey.shiklomanov@gmail.com>
2020-08-18 13:17:34 -0400

    Remove python lsp
    It's buggy, and Python works great without it.

--- init.el
+++ init.el
@@ -140,7 +140,7 @@
        ;;plantuml          ; diagrams for confusing people more
        polymode            ; multiple major modes
        ;;purescript        ; javascript, but functional
-       (python +lsp +conda)            ; beautiful is better than ugly
+       (python +conda)            ; beautiful is better than ugly
        ;;qt                ; the 'cutest' gui framework ever
        ;;racket            ; a DSL for DSLs
        ;;rest              ; Emacs as a REST client
